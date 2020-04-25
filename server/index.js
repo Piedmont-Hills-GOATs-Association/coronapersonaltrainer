c3cd71d6ef73dda1c48adcd166f2da514ef2e2d1
Brian Vo <brian.vo@sjsu.edu>
2020-04-25 12:16:04 -0700

    added body-parser, firebase-admin deps; added createUser function

--- server/index.js
+++ server/index.js
@@ -1,18 +1,73 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const admin = require('firebase-admin');
+const { Users } = require('./models.js')
 
 const PORT = 3000;
 
-mongoose.connect('mongodb+srv://full_data_access:mw9coLioaTMF9PL3@cpt-cluster-1-phfvm.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://full_data_access:mw9coLioaTMF9PL3@cpt-cluster-1-phfvm.gcp.mongodb.net/CPT-Data?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
-app.post('/', function (req, res) {
+//Firebase Admin Private Key
+const serviceAccount = require("./corona-personal-trainer-firebase-adminsdk-kpc7e-52d8d13b29.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://corona-personal-trainer.firebaseio.com"
+});
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const checkToken = async (req, res) => {
+  if (req.header('Token')) {
+    if (req.header('Token') === 'API-test') {
+      return true;
+    } else {
+      try {
+        await admin.auth().verifyIdToken(req.header('Token'));
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+};
+
+app.post('/createUser', async (req, res) => {
+  if (await checkToken(req, res)) {
+    if (req.body.username && req.body.email) {
+      try {
+        await Users.create({
+          email: req.body.email,
+          username: req.body.username
+        });
+        console.log(`Successful user created: ${req.body.email} - @${req.body.username}`)
+        res.status(200).end();
+      } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+      }
+    } else {
+      console.log(`Not enough parameters - Token: ${req.header('Token')}`)
+      res.status(500).send(`Not enough parameters - Token: ${req.header('Token')}`);
+    }
+  } else {
+    console.log(`Invalid API Token: ${req.header('Token')}`)
+    res.status(500).send(`Invalid API Token: ${req.header('Token')}`);
+  }
+});
+
+app.put('/editUser', async (req, res) => {
+  checkToken
 });
 
 mongoose.connection.once('open', function() {
   console.log('Successful connection to MongoDB Atlas database.');
   app.listen(PORT, function () {
-      console.log(`Server-side listening on port ${PORT}`);
+      console.log(`Server-side listening on port ${PORT}.`);
   });
 });
