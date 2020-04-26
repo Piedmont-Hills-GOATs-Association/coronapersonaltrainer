42c7c11eb2151ab8684b61ca33f65acc0ea1162e
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2020-04-25 17:02:02 -0700

    Edited aesthetics

--- java-client/src/homePage.java
+++ java-client/src/homePage.java
@@ -1,43 +1,54 @@
 import javax.swing.JFrame;
 import javax.swing.*;
-//import java.awt.geom.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class homePage extends JComponent{
+    static int FrameWidth = 1000;
+    static int FrameHeight = 800;
     int loginSide1 = 200;
     int loginSide2 = 60;
-    int x = 620;
-    int y = 500;
+    int x = FrameWidth / 2 - loginSide1 / 2;
+    int y = FrameHeight * 3 / 4;
+    
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
+        g2.setPaint(Color.LIGHT_GRAY);
+        Rectangle background = new Rectangle(0, 0, FrameWidth, FrameHeight);
+        g2.draw(background);
+        g2.fill(background);
+
+        g2.setPaint(new GradientPaint(x, y, Color.RED, x + loginSide1, y + loginSide2, 
+        Color.ORANGE));
         Rectangle rect = new Rectangle(x, y, loginSide1, loginSide2);
         g2.draw(rect);
+        g2.fill(rect);
         
-        Font myFont1 = new Font ("Courier New", 1, 40);
+        g2.setPaint(Color.black);
+        Font myFont1 = new Font("Roboto", Font.PLAIN, 40);
         g2.setFont(myFont1);
-        g2.drawString("Login", 660, 540);
+        g2.drawString("Login", x + loginSide1 / 4, y + loginSide2 * 3 / 4);
         
-        Font myFont2 = new Font ("Courier New", 1, 60);
+        Font myFont2 = new Font("Roboto", Font.PLAIN, 60);
         g2.setFont(myFont2);
-        g2.drawString("Welcome To", 535, 300);
+        g2.drawString("Welcome To", FrameWidth / 4 + 50, FrameHeight / 4);
 
-        Font myFont3 = new Font ("Courier New", 1, 60);
+        Font myFont3 = new Font("Roboto", Font.PLAIN, 60);
         g2.setFont(myFont3);
-        g2.drawString("Corona Personal Trainer", 300, 400);
+        g2.drawString("Corona Personal Trainer", FrameWidth / 4 - 100, FrameHeight / 2);
     }
 
     public void response(int g, int h){
-        if(g > x && g < x + side1 && h > y && h < y + side2){
+        if(g > x && g < x + loginSide1 && h > y && h < y + loginSide2){
             System.out.println("hi");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame();
-        frame.setSize(1500, 1000);
+        frame.setSize(FrameWidth, FrameHeight);
         frame.setTitle("homePage");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         homePage page = new homePage();
@@ -67,6 +78,3 @@ public class homePage extends JComponent{
     }
 
 }
-
-    
-    
